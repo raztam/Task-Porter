5632cc801e30a103ca12e31d7b8e16bec75ad233
raztamim94@gmail.com <raztamim94@gmail.com>
2025-04-10 11:20:10 +0300

    remove Google API credentials from config file for security

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-// Google API credentials - KEEP THIS FILE SECURE AND OUT OF VERSION CONTROL
-$googleapiconfig = [
-    'google_client_id' => '1021962888503-b8agiv5jm186dq7kiblpk7hdqb6qitgj.apps.googleusercontent.com',
-    'google_client_secret' => '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',
-];
