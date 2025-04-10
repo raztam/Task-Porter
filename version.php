c9cba4955cdb9d8e971313082ef4e6f15fd33cda
raztamim94@gmail.com <raztamim94@gmail.com>
2025-04-10 08:58:42 +0300

    added version file

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2024031801;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2022112800;        // Requires this Moodle version
+$plugin->component = 'local_taskporter'; // Full name of the plugin
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0.0';
